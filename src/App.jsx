6a4e89d9932d4e0518bed0ee537dcef052d63e68
Muhammed Ajmal CC <muhammedajmalcc6424094@gmail.com>
2024-12-19 16:52:34 +0530

    add user page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-
-import Sidebar from "./components/common/Sidebar";
-
+import Sidebar from "./components/common/Sidebar"
 import OverviewPage from "./pages/OverviewPage";
 import RequestPage from "./pages/RequestPage";
 import UsersPage from "./pages/UsersPage";
@@ -10,12 +8,11 @@ import OrdersPage from "./pages/OrdersPage";
 import AnalyticsPage from "./pages/AnalyticsPage";
 import SettingsPage from "./pages/SettingsPage";
 import PersonalPage from "./pages/personDetails";
-import TotallistExel from "./pages/fullDetails"
+// import TotallistExel from "./pages/fullDetails"
 
 function App() {
 	return (
 		<div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
-			{/* BG */}
 			{/* <div className='fixed inset-0 z-0'>
 				<div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80' />
 				<div className='absolute inset-0 backdrop-blur-sm' />
